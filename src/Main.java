5dcd4427d45b8837c5b3fc4b35710c7b0965676c
higgy300 <jhigueraballesteros@gmail.com>
2017-09-26 15:18:32 -0400

    added a print line to test git
    added a print line to test git (description box)

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
         int a = 5, b = 29, c = a + b;
         double x = 1.5, y = 2.0, z = x + y;
         System.out.println("result is: " + c + " and " + z);
+	System.out.println("this is a test");
 
     }
 }
