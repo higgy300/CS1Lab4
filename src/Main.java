e655a748da91cb31a5fa9a57edcda6c16e6d2b0a
higgy300 <jhigueraballesteros@gmail.com>
2017-09-26 13:27:56 -0400

    this is a commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 5, b = 29, c = a + b;
+        double x = 1.5, y = 2.0, z = x + y;
+        System.out.println("result is: " + c + " and " + z);
+
+    }
+}
